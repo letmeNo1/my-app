8bdc94c8b11ab14569265197272ed352dbb8474d
hank.huang <44250052+letmeNo1@users.noreply.github.com>
2023-03-02 15:56:42 +0800

    大的

--- src/Home.js
+++ src/Home.js
@@ -8,8 +8,23 @@ import './css/aside.css'
 import Aside from './Aside.js';
 import Section from './Section.js';
 
+import { loadProject } from "./axios/api";
 
 class Home extends React.Component {
+    componentDidMount(){
+        loadProject().then(res => {
+            this.setState({
+              isLoaded: true,
+              pageInfo: res
+            })
+            console.log(res["mThor"])
+          },(error) =>{
+            this.setState({
+              isLoaded: true,
+              pageInfo: null
+            });
+          })
+      }
     render() {
         return (
             <div>
--- src/Section.js
+++ src/Section.js
@@ -7,6 +7,12 @@ import './css/body.css';
 import './css/aside.css';
 
 class Section extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        isAnswerEditorShow: false,
+      }
+    }
     render() {
         return (
          <section class="w-90vw max-w-xl mx-auto md:px-3">
@@ -18,9 +24,6 @@ class Section extends React.Component {
                 <h1 class="text-md font-semibold mr-1">
                   test
                 </h1>
-                <span class="project-badge mr-1 bg-gray-400 text-primary">
-                  WEB
-                </span>
 
               </div>
 
@@ -31,35 +34,7 @@ class Section extends React.Component {
 
           </div>
         </div>
-
-    <div class="border-b leading-none">
-      <div class="flex justify-between md:items-center pb-2 md:pb-0 flex-col md:flex-row ">
-        <a href="/d585c28d/Demo-Project-0573f5e7a8" id="ember226" class="ember-view flex-1 block py-3 text-primary hover:no-underline hover:text-purple-600">
-          <div class="flex items-center mb-sm">
-            <h1 class="text-md font-semibold mr-1">
-              Demo Web Project
-            </h1>
-            <span class="project-badge mr-1 bg-gray-400 text-primary">
-              WEB
-            </span>
-
-            <span class="
-                label-tag project-badge mr-1">
-
-              demo
-
-            </span>
-
-          </div>
-
-          <div class="text-sm text-secondary font-semibold">
-            Updated a month ago
-          </div>
-        </a>
-
-      </div>
-    </div>
-  </section> 
+    </section> 
     );
     }
 }
